7ec83e62cb039b91f7868e62e68983eff4f91f2e
Your Name <you@example.com>
2020-01-01 22:42:39 +0500

    cart and order updated

--- app.js
+++ app.js
@@ -8,19 +8,17 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 
-const MONGODB_URI = 'mongodb+srv://Azeem_1:12345azeem@cluster0-hznol.mongodb.net/shop?';
-
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://Azeem_1:12345azeem@cluster0-hznol.mongodb.net/shop?'
 const app = express();
 const store = new MongoDBStore({
-    uri: MONGODB_URI,
-    collection: 'session'
+  uri: MONGODB_URI,
+  collection: 'sessions'
 });
 const csrfProtection = csrf();
 const port = process.env.PORT || 3000;
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -30,45 +28,33 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(
-    session({
-        secret: 'my secret',
-        resave: false,
-        saveUninitialized: false,
-        store: store
-    })
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
 );
 app.use(csrfProtection);
 app.use(flash());
-// app.use((req,res,next) => {
-//     User.findById(req.session.user._id)
-//         .then(user => {
-//             req.user = user;
-//             next();
-//     })
-// .catch(err => {
-//    console.log(err);
-// });
-// });
-
-
-
 
 app.use((req, res, next) => {
-    User.findById('5e085bf68a29d72184d69354')
-        .then(user => {
-        req.user = user;
-next();
-})
-.catch(err => console.log(err));
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
-
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use('/admin', adminRoutes);
@@ -80,11 +66,11 @@ app.use(errorController.get404);
 mongoose
     .connect(MONGODB_URI, {useNewUrlParser: true,  useUnifiedTopology: true })
     .then(result => {
-        console.log('Connected');
-        app.listen(port, () => {
-            console.log(`Server is running on ${port}`);
-        });
+    console.log('Connected');
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+    });
 })
-.catch(err => {
+  .catch(err => {
     console.log(err);
-});
+  });
